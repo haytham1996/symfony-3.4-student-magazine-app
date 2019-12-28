09ccc515d683c9c98b88a2efff779fafd614604c
haytham1996 <haytham.benmansour@esprit.tn>
2019-12-28 03:15:15 +0100

    Entity User added with the treat of newsletter , show the articles in every category

---
+++ src/ArticleBundle/Controller/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ArticleBundle\Controller;
+
+use ArticleBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/home")
+     * @Method("POST")
+     */
+    public function newAction(Request $request)
+    {
+       $user= new User();
+       /*
+       if($request->isMethod('POST') ) {
+           $user->setMail($request->get('mail')) ;
+           $em=$this->getDoctrine()->getManager() ;
+           $em->persist($user) ;
+           $em-> flush();
+           var_dump($user);
+           return $this->render('@Article/Default/index.html.twig');
+       }*/
+
+       $form=$this->createFormBuilder($user) ;
+       $form->add('mail')->add('save', SubmitType::class)->getForm();
+       $form->handleRequest($request) ;
+       if($form->isValid() && $form->isSubmitted())
+       {
+           $em=$this->getDoctrine()->getManager() ;
+           $em->persist($user) ;
+           $em->flush() ;
+           return $this->redirectToRoute('homepage');
+       }
+       return $this->render('@Article/Default/index.html.twig' , array($form->createView())) ;
+
+
+    }
+
+
+
+
+}
---
+++ src/ArticleBundle/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ArticleBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('mail')->setMethod('POST')->add('Subscribe', SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ArticleBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'articlebundle_user';
+    }
+
+
+}
